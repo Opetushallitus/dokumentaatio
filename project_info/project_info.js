5b42fd068c17d39e5662b3047bffcc9e35f854dc
Mikko Apo <mikko.apo@reaktor.fi>
2016-01-25 18:45:13 +0200

    filter_table shows project_info.js content in a table

--- project_info/project_info.js
+++ project_info/project_info.js
@@ -18,7 +18,7 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var project_infos = {};
+var project_infos = [];
 
 function reload(fn) {
   var p = path.join(dir, "**/project_info*.json")
@@ -66,6 +66,21 @@ function resolve_uses() {
   }
 }
 
+function resolve_project_infos_and_fields() {
+  var fields = [];
+  project_infos.forEach(function(i){
+    for (var property in i) {
+      if (i.hasOwnProperty(property) && fields.indexOf(property) === -1) {
+        fields.push(property)
+      }
+    }    
+  })
+  return {
+    project_infos: project_infos,
+    fields: fields
+  }
+}
+
 function uniq(arr) {
   return arr.reverse().filter(function (e, i, arr) {
     return arr.indexOf(e, i+1) === -1;
@@ -77,7 +92,15 @@ reload(startServer);
 function startServer() {
   function jsonResponse(res) { res.setHeader("Content-Type", "application/json"); }
 
-  app.get('/rest/visjs', function(req, res){
+  app.get('/filter_table.html', function(req, res){
+    res.render("filter_table", {title: "project_info.json", data: resolve_project_infos_and_fields()})
+  });
+
+  app.get('/rest/project_infos', function(req, res){
+    res.json(resolve_project_infos_and_fields())
+  });
+
+  app.get('/rest/uses', function(req, res){
     res.json(resolve_uses())
   });
 
