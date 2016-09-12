1934c78af4d55c6d22da827b38bb5b6fe6195660
Jani Rahkola <jani.rahkola@reaktor.com>
2016-09-12 15:45:55 +0300

    pg_statio script

---
+++ admin/pg_statio.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DB=$1
+HOST=$2
+QUERY="select * from pg_statio_all_tables where schemaname = 'public';"
+
+while true
+do
+    TIME=$(date --iso-8601=seconds)
+    psql -d "${DB}" -h "${HOST}" -U oph -c "${QUERY}" | \
+        tail -n+3 | \
+        head -n-2 | \
+        sed "s/\s/${TIME}, /" | \
+        sed 's/\s*|\s*/, /g'
+    sleep 1
+done
