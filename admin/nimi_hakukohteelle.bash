cb9585d48db58d696440f7f0d39163e460f11b0d
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-06-22 11:17:49 +0300

    Korjaa Caller-Idt

--- admin/nimi_hakukohteelle.bash
+++ admin/nimi_hakukohteelle.bash
@@ -7,6 +7,6 @@ fi
 
 TARJONTA_URL=https://virkailija.opintopolku.fi/tarjonta-service
 
-http GET ${TARJONTA_URL}/rest/v1/hakukohde/$1 Caller-Id:"`whoami` $0" | jq -c '{foo: (.result.oid + "|" + .result.hakukohteenNimet.kieli_fi + "|" + .result.hakukohteenNimet.kieli_sv + "|" + .result.hakukohteenNimet.kieli_en + "|" + .result.tarjoajaNimet.fi + "|" + .result.tarjoajaNimet.sv + "|" + .result.tarjoajaNimet.en + "|" + .result.hakukohteenNimi + "|" + .result.tila)}' | cut -f 4 -d '"'
+http GET ${TARJONTA_URL}/rest/v1/hakukohde/$1 Caller-Id:"`whoami`nimi.hakukohteelle.skripti" | jq -c '{foo: (.result.oid + "|" + .result.hakukohteenNimet.kieli_fi + "|" + .result.hakukohteenNimet.kieli_sv + "|" + .result.hakukohteenNimet.kieli_en + "|" + .result.tarjoajaNimet.fi + "|" + .result.tarjoajaNimet.sv + "|" + .result.tarjoajaNimet.en + "|" + .result.hakukohteenNimi + "|" + .result.tila)}' | cut -f 4 -d '"'
 
 #http GET ${TARJONTA_URL}/rest/v1/hakukohde/$1 | jq '.'
--- admin/nimi_haulle.bash
+++ admin/nimi_haulle.bash
@@ -7,5 +7,5 @@ fi
 
 TARJONTA_URL=https://virkailija.opintopolku.fi/tarjonta-service
 
-http GET ${TARJONTA_URL}/rest/v1/haku/$1 Caller-Id:"`whoami` $0"  --ignore-stdin | jq -c '[.result.oid, .result.nimi.kieli_fi, .result.hakukausiVuosi, .result.hakukausiUri, .result.ataruLomakeAvain ]'
+http GET ${TARJONTA_URL}/rest/v1/haku/$1 Caller-Id:"`whoami`.nimi.haulle.skripti"  --ignore-stdin | jq -c '[.result.oid, .result.nimi.kieli_fi, .result.hakukausiVuosi, .result.hakukausiUri, .result.ataruLomakeAvain ]'
 
